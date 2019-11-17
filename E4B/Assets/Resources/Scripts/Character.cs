613eddee7339e87af592f03790f22b75c0c9cdf9
Yash <ythacker@wustl.edu>
2019-11-16 23:33:04 -0600

    Some work done
    Sprites and inital component of game finished

---
+++ E4B/Assets/Resources/Scripts/Character.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    Rigidbody2D rigidbody;
+
+    public float health = 1f; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if (health > 0)
+            {
+                rigidbody.AddForce(Vector2.up * 7f, ForceMode2D.Impulse);
+            }
+        }
+    }
+
+    void takeDamage(float damageAmount)
+    {
+        health -= damageAmount;
+        if (health < 0)
+        {
+            Die(); 
+        }
+    }
+
+    void Die()
+    {
+        GetComponent<Rigidbody2D>().gravityScale = 0;
+        GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<ToiletPaper>())
+        {
+            takeDamage(1f);
+        }
+    }
+
+}
---
+++ E4B/Assets/Resources/Scripts/GameController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+	public static GameController instance;
+    // Start is called before the first frame update
+
+    //Outlets
+    public Transform[] spawnPoints;
+    public GameObject tp;
+
+    //Configeration
+    public float minTPDelay = 0.1f;
+    public float maxTPDelay = 5f;
+
+
+    //State tracking
+    public float timeElapsed;
+    public float TPdelay; 
+
+    void Start()
+    {
+        StartCoroutine("TPSpawnTimer");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+
+        float decreaseDelayOverTime = maxTPDelay - ((maxTPDelay - minTPDelay) / 100000 * timeElapsed);
+        TPdelay = Mathf.Clamp(decreaseDelayOverTime, minTPDelay, maxTPDelay);
+    }
+    void Awake()
+	{
+		instance = this; 
+	}
+
+    void SpawnTP()
+    {
+        Transform randomSpawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+
+        Instantiate(tp, randomSpawnPoint.position, Quaternion.identity);
+    }
+
+    IEnumerator TPSpawnTimer()
+    {
+        yield return new WaitForSeconds(TPdelay*2);
+
+        SpawnTP();
+
+        StartCoroutine("TPSpawnTimer");
+    }
+}
